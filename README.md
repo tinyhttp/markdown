82827fb70f98ee491c8358a2ed74141cd7455c22
v1rtl <pilll.pl22@gmail.com>
2021-07-10 18:38:00 +0300

    feat: move markdown to a separate library

--- README.md
+++ README.md
@@ -6,17 +6,47 @@
 
 </div>
 
-Description.
+Static markdown middleware for Node.js.
 
 ## Install
 
 ```sh
-pnpm i pkg
+pnpm i @tinyhttp/markdown
 ```
 
-[npm-url]: https://npmjs.com/package/@tinyhttp/favicon
-[github-actions]: https://github.com/tinyhttp/favicon/actions
-[gh-actions-img]: https://img.shields.io/github/workflow/status/tinyhttp/favicon/CI?style=for-the-badge&logo=github&label=&color=hotpink
-[cov-img]: https://img.shields.io/coveralls/github/tinyhttp/favicon?style=for-the-badge&color=hotpink
-[cov-url]: https://coveralls.io/github/tinyhttp/favicon
-[npm-img]: https://img.shields.io/npm/dt/@tinyhttp/favicon?style=for-the-badge&color=hotpink
+## API
+
+### `markdownStaticHandler(dir, options)`
+
+Handles static files and transforms markdown in HTML in a specified directory. It tries to assign root to `README.md` or `index.md` (and with `.markdown` extension too) in case any of them exists.
+
+#### Options
+
+- `prefix` - URL prefix to add to routes and remove from file paths
+- `stripExtension` - remove `.md` (or `.markdown`) extension from markdown files. Enabled by defaults.
+- `markedOptions` - initial [marked](https://github.com/markedjs/marked) options to be used by the handler.
+- `caching` settings for `Cache-Control` header. Disabled by default.
+
+## Example
+
+```ts
+import { App } from '@tinyhttp/app'
+import { markdownStaticHandler as md } from '@tinyhttp/markdown'
+
+new App()
+  .use(
+    md('docs', {
+      prefix: '/docs',
+      stripExtension: true,
+      markedExtensions: [{ headerIds: true }]
+    })
+  )
+  .listen(3000)
+```
+
+[npm-url]: https://npmjs.com/package/@tinyhttp/markdown
+[github-actions]: https://github.com/tinyhttp/markdown/actions
+[gh-actions-img]: https://img.shields.io/github/workflow/status/tinyhttp/markdown/CI?style=for-the-badge&logo=github&label=&color=hotpink
+[cov-img]: https://img.shields.io/coveralls/github/tinyhttp/markdown?style=for-the-badge&color=hotpink
+[cov-url]: https://coveralls.io/github/tinyhttp/markdown
+[npm-img]: https://img.shields.io/npm/dt/@tinyhttp/markdown?style=for-the-badge&color=hotpink
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,6 +1,9 @@
 import { suite } from 'uvu'
-import * as assert from 'uvu/assert'
-import { add } from '../src/index'
+import expect from 'expect'
+import path from 'path'
+import { markdownStaticHandler } from '../src/index'
+import { makeFetch } from 'supertest-fetch'
+import * as http from 'http'
 
 function describe(name: string, fn: (...args: any[]) => void) {
   const s = suite(name)
@@ -8,8 +11,157 @@ function describe(name: string, fn: (...args: any[]) => void) {
   s.run()
 }
 
-describe('add', (it) => {
-  it('adds', () => {
-    assert.equal(add(4, 5), 9)
+function createServer(
+  handler: (req: http.IncomingMessage, res: http.ServerResponse, next: () => void) => Promise<void>
+) {
+  const server = http.createServer((req, res) => {
+    handler(req, res, () => {
+      res.writeHead(404).end('Not Found')
+    })
+  })
+
+  return { fetch: makeFetch(server) }
+}
+
+const STATIC_FOLDER = path.join(process.cwd(), 'tests/fixtures')
+
+describe('Base dir', (it) => {
+  it('should scan specified directory', async () => {
+    const { fetch } = createServer(
+      markdownStaticHandler(STATIC_FOLDER, {
+        markedOptions: {
+          headerIds: false
+        }
+      })
+    )
+
+    const res = await fetch('/page')
+
+    expect((await res.text()).trim()).toContain(`<h1>Hello World</h1>`)
+
+    expect(res.status).toBe(200)
+  })
+})
+
+describe('Routing', (it) => {
+  it('should send the file matched', async () => {
+    const { fetch } = createServer(
+      markdownStaticHandler(STATIC_FOLDER, {
+        markedOptions: {
+          headerIds: false
+        }
+      })
+    )
+
+    const res = await fetch('/page')
+
+    expect((await res.text()).trim()).toContain(`<h1>Hello World</h1>`)
+
+    expect(res.status).toBe(200)
+  })
+  it('content-type should be text/html', async () => {
+    const { fetch } = createServer(markdownStaticHandler(STATIC_FOLDER, {}))
+
+    await fetch('/page').expectHeader('content-type', 'text/html; charset=utf-8')
+  })
+  it('should send 404 if no such file is found', async () => {
+    const { fetch } = createServer(markdownStaticHandler(STATIC_FOLDER, {}))
+
+    await fetch('/non-existent-page').expect(404)
+  })
+})
+
+describe('Handler options', () => {
+  describe('prefix', (it) => {
+    it('should strip prefix from paths', async () => {
+      const { fetch } = createServer(
+        markdownStaticHandler(STATIC_FOLDER, {
+          prefix: '/a'
+        })
+      )
+
+      await fetch('/a/page').expect(200)
+    })
+  })
+  describe('stripExtension', (it) => {
+    it('should send markdown files on paths with extension', async () => {
+      const { fetch } = createServer(
+        markdownStaticHandler(STATIC_FOLDER, {
+          stripExtension: false
+        })
+      )
+
+      await fetch('/page.md').expectStatus(200)
+    })
+    it('should strip extension', async () => {
+      const { fetch } = createServer(
+        markdownStaticHandler(STATIC_FOLDER, {
+          stripExtension: true
+        })
+      )
+
+      await fetch('/page').expectStatus(200)
+    })
+  })
+
+  describe('Caching', (it) => {
+    const maxAge = 3600 * 60 * 60
+    it('should disable caching by default', async () => {
+      const { fetch } = createServer(markdownStaticHandler(STATIC_FOLDER, {}))
+
+      await fetch('/page').expectHeader('Cache-Control', null)
+    })
+    it('should set maxAge header', async () => {
+      const { fetch } = createServer(
+        markdownStaticHandler(STATIC_FOLDER, {
+          caching: {
+            maxAge
+          }
+        })
+      )
+
+      await fetch('/page').expectHeader('Cache-Control', `public,max-age=${maxAge}`)
+    })
+    it('should set immutable header', async () => {
+      const { fetch } = createServer(
+        markdownStaticHandler(STATIC_FOLDER, {
+          caching: {
+            maxAge,
+            immutable: true
+          }
+        })
+      )
+
+      await fetch('/page').expectHeader('Cache-Control', `public,max-age=${maxAge},immutable`)
+    })
+    it('should add "must-revalidate" if maxAge is 0', async () => {
+      const { fetch } = createServer(
+        markdownStaticHandler(STATIC_FOLDER, {
+          caching: {
+            maxAge: 0
+          }
+        })
+      )
+
+      await fetch('/page').expectHeader('Cache-Control', `public,max-age=0,must-revalidate`)
+    })
+  })
+})
+
+describe('Index file', (it) => {
+  it('should detect index.md', async () => {
+    const { fetch } = createServer(markdownStaticHandler(`${STATIC_FOLDER}/index/md`))
+
+    await fetch('/').expect(200)
+  })
+  it('should detect index.markdown', async () => {
+    const { fetch } = createServer(markdownStaticHandler(`${STATIC_FOLDER}/index/markdown`))
+
+    await fetch('/').expect(200)
+  })
+  it('should detect README.md', async () => {
+    const { fetch } = createServer(markdownStaticHandler(`${STATIC_FOLDER}/readme/md`))
+
+    await fetch('/').expect(200)
   })
 })
